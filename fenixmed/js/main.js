f93ad3f3bde8ce69ff87db9beb478ac7787dbf81
RockStar <gagik.aproyan.dev@gmail.com>
2020-03-19 21:36:22 +0400

    fenixmed add price list page, custom select, change css styles, change js script include position

--- fenixmed/js/main.js
+++ fenixmed/js/main.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
   // open modal
 
 
+
   // close modal
   $('.modal-place-fix').on('click', function() {
     $(this).css({
@@ -23,9 +24,34 @@ $(document).ready(function() {
   // close modal
 
 
+
   // stop close modal
   $('.modal-place-center').on('click', function(e) {
     e.stopPropagation();
   });
   // stop close modal
+
+  
+
+  // check analize from price list
+  $('.all-price-item-center').on('click', function() {
+    if ($(this).data('check') === false) {
+      $(this).find('.analize-uncheked').css({
+        'display': 'none'
+      });
+      $(this).find('.analize-cheked').css({
+        'display': 'block'
+      });
+      $(this).data('check', true);
+    } else {
+      $(this).find('.analize-uncheked').css({
+        'display': 'block'
+      });
+      $(this).find('.analize-cheked').css({
+        'display': 'none'
+      });
+      $(this).data('check', false);
+    }
+  });
+  // check analize from price list
 });
