7c43e0fd4c22528d50bbf180299084b9230262f0
RockStar <gagik.aproyan.dev@gmail.com>
2020-03-25 04:03:56 +0400

    Add date and time picker, form validation, mask phone

--- fenixmed/js/main.js
+++ fenixmed/js/main.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
+  // check language
+  let lang = $('body').data('lang');
+
+  let analizeName = lang === 'ru' ? ' анализов' : lang === 'en' ? ' analysis' : ' անալիզ';
+  let analizePlaceholder = lang === 'ru' ? 'Анализов *' : lang === 'en' ? 'Analysis *' : 'Անալիզներ *';
+
+  let allError = lang === 'ru' ? 'Заполните все обязательные поля' : lang === 'en' ? 'Fill all required fields' : 'Լրացրեք բոլոր պարտադիր դաշտերը';
+  let nameError = lang === 'ru' ? 'Заполнить поле Имя' : lang === 'en' ? 'Fill Name field' : 'Լրացրեք Անուն դաշտեը';
+  let fullNameError = lang === 'ru' ? 'Заполните поле Полное Имя' : lang === 'en' ? 'Fill Fullname field' : 'Լրացրեք Ազգանուն դաշտեը';
+  let phoneEmptyError = lang === 'ru' ? 'Заполнить поле телефон' : lang === 'en' ? 'Fill Phone field' : 'Լրացրեք Հեռախոսահամար դաշտեը';
+  let phoneError = lang === 'ru' ? 'Неверный телефон' : lang === 'en' ? 'Invalid Phone' : 'Սխալ Հեռախոսահամար';
+  let emailEmptyError = lang === 'ru' ? 'Заполните поле электронной почты' : lang === 'en' ? 'Fill Email field' : 'Լրացրեք Էլ.հասցե դաշտեը';
+  let emailError = lang === 'ru' ? 'Неверный адрес электронной почты' : lang === 'en' ? 'Invalid Email' : 'Էլ.հասցեն սխալ է';
+  let branchError = lang === 'ru' ? 'Выберите поле Филиалы' : lang === 'en' ? 'Choose Branches field' : 'Ընտրեք Մասնաճյուղեր դաշտեը';
+  let analizeError = lang === 'ru' ? 'Выберите поле анализы' : lang === 'en' ? 'Choose Analysis field' : 'Ընտրեք Անալիզներ դաշտեը';
+  let dateError = lang === 'ru' ? 'Выберите поле Дня' : lang === 'en' ? 'Choose Date field' : 'Ընտրեք Ամիս ամսաթիվ դաշտեը';
+  let timeError = lang === 'ru' ? 'Выберите поле Время' : lang === 'en' ? 'Choose Time field' : 'Ընտրեք Ժամ դաշտեը';
+
+  let successText = lang === 'ru' ? 'Регистрация выполнена' : lang === 'en' ? 'Registration completed' : 'Գրանցումն ավարտված է';
+  // check language
+
+
+
   // open modal
   $('.get-analize-btn').on('click', function(e) {
     e.preventDefault();
@@ -30,6 +53,23 @@ $(document).ready(function() {
     e.stopPropagation();
   });
   // stop close modal
+
+
+
+  // check analize from price list when page is load
+  if ($('.all-price-item-center')) {
+    $('.all-price-item-center').each(function() {
+      if ($(this).data('check') === true) {
+        $(this).find('.analize-uncheked').css({
+          'display': 'none'
+        });
+        $(this).find('.analize-cheked').css({
+          'display': 'block'
+        });
+      }
+    });
+  }
+  // check analize from price list when page is load
 
   
 
@@ -175,6 +215,7 @@ $(document).ready(function() {
 
 
   // select form analize
+  let selectAnalizeArray = [];
   $('.form-analize-list-item').on('click', function() {
     if ($(this).data('check') === false) {
       $(this).css({
@@ -187,7 +228,8 @@ $(document).ready(function() {
         'display': 'block'
       });
       $(this).data('check', true);
-      // TODO: Add in analize array for send
+      selectAnalizeArray.push($(this).data('analizeId'));
+      $('.form-analize-text').text(selectAnalizeArray.length + analizeName);
     } else {
       $(this).css({
         'background-color': 'var(--header-footer-bg-color)'
@@ -199,8 +241,275 @@ $(document).ready(function() {
         'display': 'none'
       });
       $(this).data('check', false);
-      /// TODO: Remove in analize array for send
+      selectAnalizeArray = selectAnalizeArray.filter(item => {
+        return item !== $(this).data('analizeId');
+      });
+      if (selectAnalizeArray.length > 0) {
+        $('.form-analize-text').text(selectAnalizeArray.length + analizeName);
+      } else {
+        $('.form-analize-text').text(analizePlaceholder);
+      }
     }
   });
   // select form analize
+
+
+
+  // check selected form analize when page is load
+  if ($('.form-analize-list-item')) {
+    $('.form-analize-list-item').each(function() {
+      if ($(this).data('check') === true) {
+        $(this).css({
+          'background-color': 'rgba(0, 0, 0, 0.1)'
+        });
+        $(this).find('.analize-uncheked').css({
+          'display': 'none'
+        });
+        $(this).find('.analize-cheked').css({
+          'display': 'block'
+        });
+        selectAnalizeArray.push($(this).data('analizeId'));
+        $('.form-analize-text').text(selectAnalizeArray.length + analizeName);
+      }
+    });
+  }
+  // check selected form analize when page is load
+
+
+
+  // remove error border, clear error text, with typing
+  $('.register-input').on('keypress keydown keyup', function() {
+    $(this).css({
+      'border': '2px solid var(--green-block-border-decor)'
+    });
+    $('.form-error-message').text('');
+  });
+  $('.select-items').on('click', function() {
+    $('.select-selected').css({
+      'border': '2px solid var(--green-block-border-decor)'
+    });
+    $('.form-error-message').text('');
+  });
+  $('.form-analize-list-item').on('click', function() {
+    $('.form-analize-place').css({
+      'border': '2px solid var(--green-block-border-decor)'
+    });
+    $('.form-error-message').text('');
+  });
+  $('#datepicker').on('change', function() {
+    $('.ui-datepicker-trigger').css({
+      'border': '2px solid var(--green-block-border-decor)'
+    });
+    $('.form-error-message').text('');
+  });
+  $('#timepicker').on('change', function() {
+    $('.input-btn').css({
+      'border': '2px solid var(--green-block-border-decor)'
+    });
+    $('.form-error-message').text('');
+  });
+  $('.short-input').on('change', function() {
+    $(this).css({
+      'border': '2px solid var(--green-block-border-decor)'
+    });
+    $('.form-error-message').text('');
+  });
+  // remove error border, clear error text, with typing
+
+
+
+  // email validation with regex
+  function validateEmail(email) {
+    let regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regexEmail.test(String(email).toLowerCase());
+  }
+  // email validation with regex
+
+
+
+  // phone validation with regex
+  function validatePhone(phone) {
+      var reg = /^(0)((33)|4(1|3|4|9)|(55)|(77)|(9([1|3-6]|[8-9])))[\-]?[0-9]{2}[\-]?[0-9]{2}[\-]?[0-9]{2}$/;
+      return reg.test(String(phone).toLowerCase());
+  }
+  // phone validation with regex
+
+
+
+  // send form data to backend
+  $('.form-btn').on('click', function() {
+    let name = $('#name').val();
+    let fullName = $('#fullname').val();
+    let phone = $('#phone').val();
+    let email = $('#email').val();
+    let branch = $('#branch').val();
+    let analizes = selectAnalizeArray;
+    let datepicker = $('#datepicker').val();
+    let timepicker = $('#timepicker').val();
+    let message = $('#message').val();
+
+    if (name === '' && fullName === '' && phone === '' && email === '' && branch === '0' && analizes.length <= 0 && datepicker === '' && timepicker === '') {
+      $('.form-error-message').text(allError);
+      $('.register-input').css({
+        'border': '2px solid var(--error-red)'
+      });
+      $('.select-selected').css({
+        'border': '2px solid var(--error-red)'
+      });
+      $('.form-analize-place').css({
+        'border': '2px solid var(--error-red)'
+      });
+      $('.ui-datepicker-trigger').css({
+        'border': '2px solid var(--error-red)'
+      });
+      $('.input-btn').css({
+        'border': '2px solid var(--error-red)'
+      });
+      return;
+    }
+
+    if (name === '') {
+      $('.form-error-message').text(nameError);
+      $('#name').css({
+        'border': '2px solid var(--error-red)'
+      });
+      return;
+    }
+
+    if (fullName === '') {
+      $('.form-error-message').text(fullNameError);
+      $('#fullname').css({
+        'border': '2px solid var(--error-red)'
+      });
+      return;
+    }
+
+    if (phone === '') {
+      $('.form-error-message').text(phoneEmptyError);
+      $('#phone').css({
+        'border': '2px solid var(--error-red)'
+      });
+      return;
+    }
+
+    if (!validatePhone(phone)) {
+      $('.form-error-message').text(phoneError);
+      $('#phone').css({
+        'border': '2px solid var(--error-red)'
+      });
+      return;
+    }
+
+    if (email === '') {
+      $('.form-error-message').text(emailEmptyError);
+      $('#email').css({
+        'border': '2px solid var(--error-red)'
+      });
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      $('.form-error-message').text(emailError);
+      $('#email').css({
+        'border': '2px solid var(--error-red)'
+      });
+      return;
+    }
+
+    if (branch === '0') {
+      $('.form-error-message').text(branchError);
+      $('.select-selected').css({
+        'border': '2px solid var(--error-red)'
+      });
+      return;
+    }
+
+    if (analizes.length <= 0) {
+      $('.form-error-message').text(analizeError);
+      $('.form-analize-place').css({
+        'border': '2px solid var(--error-red)'
+      });
+      return;
+    }
+
+    if (datepicker === '') {
+      $('.form-error-message').text(dateError);
+      $('#datepicker').css({
+        'border': '2px solid var(--error-red)'
+      });
+      $('.ui-datepicker-trigger').css({
+        'border': '2px solid var(--error-red)'
+      });
+      return;
+    }
+
+    if (timepicker === '') {
+      $('.form-error-message').text(timeError);
+      $('#timepicker').css({
+        'border': '2px solid var(--error-red)'
+      });
+      $('.input-btn').css({
+        'border': '2px solid var(--error-red)'
+      });
+      return;
+    }
+    
+    $.ajax({
+      method: 'POST',
+      url: url,
+      data: {
+        name,
+        fullName,
+        phone,
+        email,
+        branch,
+        analizes,
+        datepicker,
+        timepicker,
+        message
+      },
+      success: function() {
+        $('#name').val('');
+        $('#fullname').val('');
+        $('#phone').val('');
+        $('#email').val('');
+        $('#branch').val('0');
+        selectAnalizeArray = [];
+        $('#datepicker').val('');
+        $('#timepicker').val('');
+        $('#message').val('');
+        
+        $('.register-input').css({
+          'border': '2px solid var(--green-block-border-decor)'
+        });
+        $('.select-selected').css({
+          'border': '2px solid var(--green-block-border-decor)'
+        });
+        $('.form-analize-place').css({
+          'border': '2px solid var(--green-block-border-decor)'
+        });
+        $('.ui-datepicker-trigger').css({
+          'border': '2px solid var(--green-block-border-decor)'
+        });
+        $('.input-btn').css({
+          'border': '2px solid var(--green-block-border-decor)'
+        });
+        $('.short-input').css({
+          'border': '2px solid var(--green-block-border-decor)'
+        });
+
+        $('.form-success-message').text(successText);
+        setTimeout(function() {
+          $('.form-success-message').text('');
+        }, 3000);
+      },
+      error: function(res) {
+        $('.form-error-message').text(res);
+        setTimeout(function() {
+          $('.form-error-message').text('');
+        }, 3000);
+      }
+    });
+  });
+  // send form data to backend
 });
