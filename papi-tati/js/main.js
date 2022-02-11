bcd619ce87c99dedb700897f96a6dedc4ee98ac6
Gagik Aproyan <gagik.aproyan.dev@gmail.com>
2022-02-11 17:42:24 +0400

    Add: papi tati love story

---
+++ papi-tati/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('#sb-slider').slicebox({
+    speed: 900,
+    autoplay: true,
+    interval: 5000,
+    orientation : 'r',
+    disperseFactor : 30,
+    cuboidsRandom : true,
+  });
+});
