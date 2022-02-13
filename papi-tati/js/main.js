d17ece3f9582a72fea78fe0c71e7343c2b4be0c6
Gagik Aproyan <gagik.aproyan.dev@gmail.com>
2022-02-13 14:45:47 +0400

    Add: new slide effect

--- papi-tati/js/main.js
+++ papi-tati/js/main.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-  $('#sb-slider').slicebox({
-    speed: 900,
-    autoplay: true,
-    interval: 5000,
-    orientation : 'r',
-    disperseFactor : 30,
-    cuboidsRandom : true,
-  });
+  const elements = document.querySelectorAll('.animation-elem');
+  let upDown = true;
+  let elemId = 0;
+
+  setInterval(function() {
+    if (upDown === true) {
+      $(elements[elemId]).css({
+        'clip-path': 'circle(100% at center)',
+      });
+      elemId++;
+      if (elemId === 54) {
+        upDown = false;
+      }
+    } else {
+      elemId--;
+      $(elements[elemId]).css({
+        'clip-path': 'circle(0% at center)',
+      });
+      if (elemId === 0) {
+        upDown = true;
+      }
+    }
+  }, 8000);
 });
